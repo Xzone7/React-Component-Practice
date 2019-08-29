02bbbfc10d9f11ce4b3de155fd6d5bdefc0e70ed
Xu Chen <cxghy2011@gmail.com>
2019-08-29 13:28:57 -0700

    Add project container component

--- src/components/Project-1/index.js
+++ src/components/Project-1/index.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStateToProps from '../../redux/config/mapStateToProps';
 import RedirectToLogin from '../RedirectToLogin/RedirectToLogin';
+import axios from 'axios';
 
 class Project_1 extends Component {
     constructor(props) {
         super(props);
     }
 
+    componentDidMount() {
+
+    }
+
     render() {
         const authentication = this.props.authentication;
         return (
             authentication ?
-                <div>Hello</div>
+                <div>
+                </div>
                 :
                 <RedirectToLogin />
         );
